48046cfa226eef5aaa89fd0cc0d591e4eaaedbb3
stickler-ci <support@stickler-ci.com>
2019-09-13 21:16:16 +0000

    Fixing style errors.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,47 +1,49 @@
+# frozen_string_literal: true
+
 require './lib/main'
 
 RSpec.describe Enumerable do
-  describe "#my_count" do
-    it "calls a block on an object and returns the number of elements" do
+  describe '#my_count' do
+    it 'calls a block on an object and returns the number of elements' do
       arr = [1, 2, 3, 4, 5]
       expect(arr.my_count).to eql(5)
     end
   end
-    
-  describe "#my_map" do
-    it "returns a new array as a result of calling a block once on each item in the original array" do
+
+  describe '#my_map' do
+    it 'returns a new array as a result of calling a block once on each item in the original array' do
       arr = [1, 2, 3]
       result = [2, 4, 6]
-      expect(arr.my_map {|x| x * 2}).to(eql(result))
+      expect(arr.my_map { |x| x * 2 }).to(eql(result))
     end
   end
-    
-  describe "#my_any?" do
-    it "returns true if the block passed returns true for any item in the array" do
+
+  describe '#my_any?' do
+    it 'returns true if the block passed returns true for any item in the array' do
       arr = [1, 2, 3]
-      expect(arr.my_any? {|x| x > 2}).to(eql(true))
+      expect(arr.my_any? { |x| x > 2 }).to(eql(true))
     end
   end
-    
-  describe "#my_none?" do
-    it "returns true if the block passed returns false for each item in the array" do
+
+  describe '#my_none?' do
+    it 'returns true if the block passed returns false for each item in the array' do
       arr = [1, 2, 3]
-      expect(arr.my_none? {|x| x > 4}).to(eql(true))
+      expect(arr.my_none? { |x| x > 4 }).to(eql(true))
     end
   end
-    
-  describe "#my_select" do
-    it "returns an array containing the items that return through when a block is passed to them" do
+
+  describe '#my_select' do
+    it 'returns an array containing the items that return through when a block is passed to them' do
       arr = [1, 2, 3, 4]
       result = [2, 4]
-      expect(arr.my_select {|x| x % 2 == 0}).to(eql(result))
+      expect(arr.my_select(&:even?)).to(eql(result))
     end
   end
-    
-  describe "#my_inject" do
-    it "combines all elements by applying a binary function specified in a block" do
+
+  describe '#my_inject' do
+    it 'combines all elements by applying a binary function specified in a block' do
       arr = [1, 2, 3]
-      expect(arr.my_inject {|x,y| x + y}).to(eql(6))
+      expect(arr.my_inject { |x, y| x + y }).to(eql(6))
     end
   end
 end
