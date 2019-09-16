4d8e1fecba960b628c250502e514eda2666766d9
Marvellous Michaels <marvellousubani@gmail.com>
2019-09-16 17:12:45 +0100

    Fixed Testing Issues

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,47 +3,66 @@
 require './lib/main'
 
 RSpec.describe Enumerable do
+  let(:arr){[1, 2, 3]}
+
   describe '#my_count' do
     it 'calls a block on an object and returns the number of elements' do
-      arr = [1, 2, 3, 4, 5]
-      expect(arr.my_count).to eql(5)
+      expect(arr.my_count).to eql(3)
     end
   end
 
   describe '#my_map' do
-    it 'returns a new array as a result of calling a block once on each item in the original array' do
-      arr = [1, 2, 3]
+    it 'returns a new array as a result of calling a block once on each item in the original array (multiplication case)' do
       result = [2, 4, 6]
       expect(arr.my_map { |x| x * 2 }).to(eql(result))
     end
+
+    it 'returns a new array as a result of calling a block once on each item in the original array (addition case)' do
+      result = [3, 4, 5]
+      expect(arr.my_map { |x| x + 2 }).to(eql(result))
+    end
+
+    it 'returns a new array as a result of calling a block once on each item in the original array (subtraction case)' do
+      result = [-1, 0, 1]
+      expect(arr.my_map { |x| x - 2 }).to(eql(result))
+    end
+
   end
 
   describe '#my_any?' do
     it 'returns true if the block passed returns true for any item in the array' do
-      arr = [1, 2, 3]
       expect(arr.my_any? { |x| x > 2 }).to(eql(true))
+    end
+    it 'returns false if the block passed returns false for all item in the array' do
+      expect(arr.my_any? {|x| x > 3 }).to(eql(false))
     end
   end
 
   describe '#my_none?' do
     it 'returns true if the block passed returns false for each item in the array' do
-      arr = [1, 2, 3]
       expect(arr.my_none? { |x| x > 4 }).to(eql(true))
+    end
+    it 'returns false if the block passed returns true for any item in the array' do
+      expect(arr.my_none? {|x| x > 2}).to(eql(false))
+    end
+    it 'returns false if the block passed returns true for all items in the array' do
+      expect(arr.my_none? {|x| x < 4}).to(eql(false))
     end
   end
 
   describe '#my_select' do
-    it 'returns an array containing the items that return through when a block is passed to them' do
-      arr = [1, 2, 3, 4]
-      result = [2, 4]
+    it 'returns an array containing the items that return true when a block is passed to them' do
+      result = [2]
       expect(arr.my_select(&:even?)).to(eql(result))
     end
   end
 
   describe '#my_inject' do
-    it 'combines all elements by applying a binary function specified in a block' do
-      arr = [1, 2, 3]
+    it 'combines all elements by applying a binary function specified in a block (addition case)' do
       expect(arr.my_inject { |x, y| x + y }).to(eql(6))
+    end
+    it 'combines all elements by applying a binary function specified in a block (multiplication case)' do
+      expect(arr.my_inject { |x, y| x * y }).to(eql(6))
     end
   end
 end
